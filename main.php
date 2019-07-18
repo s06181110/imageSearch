e81688904fbaba085fa256d00a325a6125ddde53
s06181110 <s06181110@gmail.com>
2019-07-18 19:03:42 +0900

    ページ上部へ移動するボタンを作成

--- main.php
+++ main.php
@@ -10,6 +10,7 @@
 <header>
     <a class="link" href="toppage.php"><h1>人物写真検索システム</h1></a>
 </header>
+<a id="pageTop" href="#"><div class="nav">topへ</div></a>
 <?php
 //require 'ProenDB.php';
 require 'form.php'
@@ -48,6 +49,38 @@ $(function(){
         $('.js-modal').fadeOut();
         return false;
     });
+});
+
+$(function(){
+    var topBtn=$('#pageTop');
+    topBtn.hide();
+
+//◇ボタンの表示設定
+    $(window).scroll(function(){
+        if($(this).scrollTop()>80){
+
+            //---- 画面を80pxスクロールしたら、ボタンを表示する
+            topBtn.fadeIn();
+
+        }else{
+
+            //---- 画面が80pxより上なら、ボタンを表示しない
+            topBtn.fadeOut();
+
+        }
+    });
+
+
+
+// ◇ボタンをクリックしたら、スクロールして上に戻る
+    topBtn.click(function(){
+        $('body,html').animate({
+            scrollTop: 0},500);
+        return false;
+
+    });
+
+
 });
 
 </script>
